2efe21cf8119f813cad4c0ad32184315af134a5b
Rafał Ochman <rafalochman99@gmail.com>
2021-07-30 21:22:37 +0200

    add change password functionality

---
+++ Data/ChangePasswordInput.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace invMed.Data
+{
+    public class ChangePasswordInput
+    {
+        [Required]
+        [StringLength(36, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        [RegularExpression(@"^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{6,}$", ErrorMessage = "The password must contain at lest one uppercase, lowercase, special character and numeric value.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Current password")]
+        public string CurrentPassword { get; set; }
+
+        [Required]
+        [StringLength(36, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        [RegularExpression(@"^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{6,}$", ErrorMessage = "The password must contain at lest one uppercase, lowercase, special character and numeric value.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "New password")]
+        public string NewPassword { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm new password")]
+        [Compare("NewPassword", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmNewPassword { get; set; }
+    }
+}
--- Services/AccountService.cs
+++ Services/AccountService.cs
@@ -11,11 +11,13 @@ namespace invMed.Services
     {
         private readonly ApplicationDbContext _db;
         private readonly UserManager<AspNetUser> _userManager;
+        private readonly SignInManager<AspNetUser> _signInManager;
 
-        public AccountService(ApplicationDbContext db, UserManager<AspNetUser> userManager)
+        public AccountService(ApplicationDbContext db, UserManager<AspNetUser> userManager, SignInManager<AspNetUser> signInManager)
         {
             _db = db;
             _userManager = userManager;
+            _signInManager = signInManager;
         }
 
         public async Task<UserView> GetUserByName(string userName)
@@ -23,6 +25,29 @@ namespace invMed.Services
             var user = await _db.Users.FirstOrDefaultAsync(u => u.UserName == userName);
             var role = await _userManager.GetRolesAsync(user);
             return new UserView() { Id = user.Id, Name = user.Name, Surname = user.Surname, UserName = user.UserName, Email = user.Email, Role = role[0] };
+        }
+
+        public async Task<bool> ChangeUserPassword(string userName, string currentPassword, string newPassword)
+        {
+            var user = await _userManager.FindByNameAsync(userName);
+            var passwordValidation = await _signInManager.UserManager.CheckPasswordAsync(user, currentPassword);
+            if (passwordValidation)
+            {
+                try
+                {
+                    var token = await _userManager.GeneratePasswordResetTokenAsync(user);
+                    var result = await _userManager.ResetPasswordAsync(user, token, newPassword);
+                    return result.Succeeded;
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
