9373576baaec79810568c1adfbeefffe1767ae48
Rafał Ochman <rafalochman99@gmail.com>
2021-08-15 19:51:24 +0200

    add add product dialog

---
+++ Data/Imputs/Products/AddProductInput.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace invMed.Data
+{
+    public class AddProductInput
+    {
+        [Required(ErrorMessage = "Pole Nazwa jest wymagane.")]
+        [Display(Name = "Nazwa")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Pole Kategoria jest wymagane.")]
+        [Display(Name = "Kategoria")]
+        public string Category { get; set; }
+
+        [Required(ErrorMessage = "Pole Producent jest wymagane.")]
+        [Display(Name = "Producent")]
+        public string Producer { get; set; }
+
+        [Required(ErrorMessage = "Pole Dostawca jest wymagane.")]
+        [Display(Name = "Dostawca")]
+        public string Supplier { get; set; }
+
+        [Required(ErrorMessage = "Pole Cena jest wymagane.")]
+        [Display(Name = "Cena")]
+        public double Price { get; set; }
+
+        [Required(ErrorMessage = "Pole Ilość minimalna jest wymagane.")]
+        [Display(Name = "Ilość minimalna")]
+        public int MinAmount { get; set; }
+
+        [Required(ErrorMessage = "Pole Ilość maksymalna jest wymagane.")]
+        [Display(Name = "Ilość maksymalna")]
+        public int MaxAmount { get; set; }
+    }
+}
--- Services/ProductsService.cs
+++ Services/ProductsService.cs
@@ -19,12 +19,27 @@ namespace invMed.Services
 
         public async Task<List<Product>> GetProducts()
         {
-          return await _db.Products.OrderBy(x => x.Category).ToListAsync();
+            return await _db.Products.OrderBy(x => x.Category).ToListAsync();
         }
 
         public async Task<IEnumerable<string>> Search(string searchValue)
         {
             return await _db.Products.Where(x => x.Name.Contains(searchValue)).Select(x => x.Name).ToArrayAsync();
+        }
+
+        public async Task<bool> AddProduct(AddProductInput input)
+        {
+            var product = new Product() { Name = input.Name, Category = input.Category, Amount = 0, Producer = input.Producer, Supplier = input.Supplier, Price = input.Price, MinAmount = input.MinAmount, MaxAmount = input.MaxAmount };
+            try
+            {
+                await _db.Products.AddAsync(product);
+                await _db.SaveChangesAsync();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
         }
     }
 }
