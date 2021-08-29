4ee5f072f971145ec65ec373f0467cbf1309c421
Rafał Ochman <rafalochman99@gmail.com>
2021-08-29 19:30:51 +0200

    add search functionality on products page

--- Services/ProductsService.cs
+++ Services/ProductsService.cs
@@ -23,9 +23,33 @@ namespace invMed.Services
             return await _db.Products.OrderBy(x => x.Category).ToListAsync();
         }
 
-        public async Task<IEnumerable<string>> Search(string searchValue)
+        public async Task<IEnumerable<SearchDto>> Search(string searchValue)
         {
-            return await _db.Products.Where(x => x.Name.Contains(searchValue)).Select(x => x.Name).ToArrayAsync();
+            var products = await _db.Products.Where(x => x.Name.Contains(searchValue)).ToListAsync();
+            var items = await _db.Items.Where(x => x.BarCode.Contains(searchValue)).ToListAsync();
+            var searchDtos = new List<SearchDto>();
+            foreach( var product in products)
+            {
+                searchDtos.Add(new SearchDto()
+                {
+                    Id = product.Id,
+                    Type = "product",
+                    Name = product.Name,
+                    Category = product.Category
+                });
+            }
+            foreach(var item in items)
+            {
+                searchDtos.Add(new SearchDto()
+                {
+                    Id = item.Id,
+                    Type = "item",
+                    Name = item.Product.Name,
+                    Category = item.Product.Category,
+                    Barcode = item.BarCode
+                });
+            }
+            return searchDtos;
         }
 
         public async Task<bool> AddProduct(AddProductInput input)
