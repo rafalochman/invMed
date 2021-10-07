9c8e78f3ba33502fc7893be9634106420ca226d5
Rafał Ochman <rafalochman99@gmail.com>
2021-10-07 22:45:41 +0200

    add authorization to pages and controls

---
+++ Data/Enums/AuthorizeRoleName.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace invMed.Data.Enums
+{
+    public static class AuthorizeRoleName
+    {
+        public const string Admin = "Admin";
+        public const string Manager = "Manager";
+        public const string Warehouseman = "Magazynier";
+        public const string WarehousemanAndManager = "Magazynier, Manager";
+    }
+}
