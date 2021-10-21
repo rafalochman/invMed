4f0ac33546b5709e7961210d99a0c4291b48d89e
Rafał Ochman <rafalochman99@gmail.com>
2021-10-21 15:56:00 +0200

    log account service exceptions

--- Services/AccountService.cs
+++ Services/AccountService.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using invMed.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
@@ -24,13 +25,27 @@ namespace invMed.Services
         public async Task<UserView> GetUserByName(string userName)
         {
             var user = await _db.Users.FirstOrDefaultAsync(u => u.UserName == userName);
-            var role = await _userManager.GetRolesAsync(user);
-            return new UserView() { Id = user.Id, Name = user.Name, Surname = user.Surname, UserName = user.UserName, Email = user.Email, Role = role[0] };
+            try
+            {
+                var role = await _userManager.GetRolesAsync(user);
+                return new UserView() { Id = user.Id, Name = user.Name, Surname = user.Surname, UserName = user.UserName, Email = user.Email, Role = role[0] };
+            }
+            catch(Exception ex)
+            {
+                _logger.LogError(ex, "Get user by name error.");
+                return new UserView();
+            }
+            
         }
 
         public async Task<bool> ChangeUserPassword(string userName, string currentPassword, string newPassword)
         {
             var user = await _userManager.FindByNameAsync(userName);
+            if(user is null)
+            {
+                _logger.LogError("Change user password error - user not found.");
+                return false;
+            }
             var passwordValidation = await _signInManager.UserManager.CheckPasswordAsync(user, currentPassword);
             if (passwordValidation)
             {
@@ -40,13 +55,15 @@ namespace invMed.Services
                     var result = await _userManager.ResetPasswordAsync(user, token, newPassword);
                     return result.Succeeded;
                 }
-                catch
+                catch (Exception ex)
                 {
+                    _logger.LogError(ex, "Change user password error.");
                     return false;
                 }
             }
             else
             {
+                _logger.LogError("Change user password error - password not valid.");
                 return false;
             }
         }
