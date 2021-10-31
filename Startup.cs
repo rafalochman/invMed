e0047285e0245b85dae9416119df5ca1e21da35b
Rafał Ochman <rafalochman99@gmail.com>
2021-10-31 19:19:39 +0100

    translate error messages

--- Startup.cs
+++ Startup.cs
@@ -31,8 +31,6 @@ namespace invMed
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
@@ -87,7 +85,6 @@ namespace invMed
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, UserManager<AspNetUser> userManager)
         {
             if (env.IsDevelopment())
@@ -100,7 +97,6 @@ namespace invMed
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
